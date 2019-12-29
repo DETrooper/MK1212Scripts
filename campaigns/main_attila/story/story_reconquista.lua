7175b7515f649317634485a2eae296f7bfddf281
DETrooper <detrooper554@yahoo.com>
2019-12-28 21:00:21 -0500

    Add files via upload

--- campaigns/main_attila/story/story_reconquista.lua
+++ campaigns/main_attila/story/story_reconquista.lua
@@ -13,6 +13,10 @@ NAVARRE_KEY = "mk_fact_navarre";
 PORTUGAL_KEY = "mk_fact_portugal";
 RECONQUISTA_MISSION_TURN = 3;
 
+ARAGON_RELIGION = "att_rel_chr_catholic";
+CASTILE_RELIGION = "att_rel_chr_catholic";
+NAVARRE_RELIGION = "att_rel_chr_catholic";
+PORTUGAL_RELIGION = "att_rel_chr_catholic";
 RECONQUISTA_COMPLETE = false;
 
 REGIONS_SPAIN_RECONQUISTA = {
@@ -46,28 +50,56 @@ function Add_Reconquista_Story_Events_Listeners()
 end
 
 function FactionTurnStart_Reconquista(context)
-	local turn_number = cm:model():turn_number();
-	local turn_faction = context:faction():name();
+	local faction_key = context:faction():name();
 
-	if turn_faction == ARAGON_KEY or turn_faction == CASTILE_KEY or turn_faction == NAVARRE_KEY or turn_faction == PORTUGAL_KEY then
+	if faction_key == ARAGON_KEY or faction_key == CASTILE_KEY or faction_key == NAVARRE_KEY or faction_key == PORTUGAL_KEY then
+		local turn_number = cm:model():turn_number();
 		local regions_catholic = Are_Regions_Religion("att_rel_chr_catholic", REGIONS_SPAIN_RECONQUISTA);
 
-		if turn_number == RECONQUISTA_MISSION_TURN and cm:model():world():faction_by_key(turn_faction):is_human() then
-			cm:trigger_mission(turn_faction, "mk_mission_story_reconquista_spain");
+		if context:faction():is_human() then
+			if turn_number == RECONQUISTA_MISSION_TURN and context:faction():state_religion() == "att_rel_chr_catholic" then
+				cm:trigger_mission(faction_key, "mk_mission_story_reconquista_spain");
+			elseif turn_number > RECONQUISTA_MISSION_TURN and context:faction():state_religion() ~= "att_rel_chr_catholic" then
+				if faction_key == ARAGON_KEY and ARAGON_RELIGION == "att_rel_chr_catholic" then
+					cm:override_mission_succeeded_status(ARAGON_KEY, "mk_mission_story_reconquista_spain", false);
+					ARAGON_RELIGION = context:faction():state_religion();
+				elseif faction_key == CASTILE_KEY and CASTILE_RELIGION == "att_rel_chr_catholic" then
+					cm:override_mission_succeeded_status(CASTILE_KEY, "mk_mission_story_reconquista_spain", false);
+					CASTILE_RELIGION = context:faction():state_religion();
+				elseif faction_key == NAVARRE_KEY and NAVARRE_RELIGION == "att_rel_chr_catholic" then
+					cm:override_mission_succeeded_status(NAVARRE_KEY, "mk_mission_story_reconquista_spain", false);
+					NAVARRE_RELIGION = context:faction():state_religion();
+				elseif faction_key == PORTUGAL_KEY and PORTUGAL_RELIGION == "att_rel_chr_catholic" then
+					cm:override_mission_succeeded_status(PORTUGAL_KEY, "mk_mission_story_reconquista_spain", false);
+					PORTUGAL_RELIGION = context:faction():state_religion();
+				end
+			end
 		end
 		
 		if regions_catholic == true and RECONQUISTA_COMPLETE == false then
 			RECONQUISTA_COMPLETE = true;
 
-			cm:override_mission_succeeded_status(ARAGON_KEY, "mk_mission_story_reconquista_spain", true);
-			cm:override_mission_succeeded_status(CASTILE_KEY, "mk_mission_story_reconquista_spain", true);
-			cm:override_mission_succeeded_status(NAVARRE_KEY, "mk_mission_story_reconquista_spain", true);
-			cm:override_mission_succeeded_status(PORTUGAL_KEY, "mk_mission_story_reconquista_spain", true);
+			if ARAGON_RELIGION == "att_rel_chr_catholic" then
+				cm:override_mission_succeeded_status(ARAGON_KEY, "mk_mission_story_reconquista_spain", true);
+			end
+
+			if CASTILE_RELIGION == "att_rel_chr_catholic" then
+				cm:override_mission_succeeded_status(CASTILE_KEY, "mk_mission_story_reconquista_spain", true);
+			end	
+
+			if NAVARRE_RELIGION == "att_rel_chr_catholic" then
+				cm:override_mission_succeeded_status(NAVARRE_KEY, "mk_mission_story_reconquista_spain", true);
+			end
+
+			if PORTUGAL_RELIGION == "att_rel_chr_catholic" then
+				cm:override_mission_succeeded_status(PORTUGAL_KEY, "mk_mission_story_reconquista_spain", true);
+			end
 
 			local faction_list = cm:model():world():faction_list();
 
 			for i = 0, faction_list:num_items() - 1 do
 				local current_faction = faction_list:item_at(i);
+				
 				cm:show_message_event(
 					current_faction:name(),
 					"message_event_text_text_mk_event_spa_reconquista_title", 
